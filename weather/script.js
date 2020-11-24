d23469a150893cc8be03b6f5d51cf839bedd0056
Abiona Quadrat Adewemimo <70848491+abiona01@users.noreply.github.com>
2020-11-25 00:58:26 +0100

    Add files via upload

---
+++ weather/script.js
@@ -0,0 +1,43 @@
+const key = '0e31b5839a4b10fe945b80608ef09c34'
+const formEl = document.querySelector('form');
+const details = document.querySelector('.details');
+
+formEl.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    details.innerHTML = '<h1>Loading...</h1>';
+    const location  = e.target.location.value;
+    weatherApp(location);
+});
+
+async function weatherApp(location){
+    const data = await fetchAPI(location);
+    generateHTML(data);
+}
+
+
+async function fetchAPI(location){
+    const baseURL = `http://api.weatherstack.com/current?access_key=${key}&query=${location}`;
+    const res = await fetch(baseURL);
+    const data = await res.json(); 
+    console.log(data);
+    return data;
+
+}
+
+function generateHTML(data){
+    const html = `
+    <h3 class="temp grey-text text-lighten-2">${data.current.temperature}&deg;</h3>
+                <h3 class="status grey-text text-lighten-2">${data.current.weather_descriptions.map(item => item).join(" ")}</h3>
+                <div class="query">${data.request.query}</div>
+                
+    `;
+    details.innerHTML = html;
+}
+
+
+
+
+
+
+
+
